3b3f7c6c62403d60f9024a88744a7f760c859e81
Wuqian Ye <natsumi3613@qq.com>
2017-07-11 19:27:27 +0000

    #home

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // JavaScript File
 $( document ).ready(function() {
+    $('ul li').click(function(){
+    var clicked_tab = $(this);
+    clicked_tab.addClass('active');
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
+    var clicked_href = $(this).children('a').attr('href');
+    $('img').hide();
+    $(clicked_href).show();
+});
+});
