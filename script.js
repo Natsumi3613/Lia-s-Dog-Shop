b942d2c31a9a1eaeae11d843f9cdac13c42f9af3
Wuqian Ye <natsumi3613@qq.com>
2017-07-21 15:12:42 +0000

    #

--- script.js
+++ script.js
@@ -9,24 +9,99 @@ $( document ).ready(function() {
     $("#pic3").click(function(){
         $("#gallery-main").attr("src","http://2.bp.blogspot.com/-RSo7DR8Yipk/ToJ8dzbStTI/AAAAAAAACbk/qM0NN5oXlUQ/s400/golden-retriever-puppies%25281%2529.jpg");
     });
+    $("#suppliesbtn").click(function(){
+        $("#cart").hide();
+    });
     $("#cartbtn").click(function(){
         $("#supplies").hide();
         $("#cart").show();
     });
-    $("#suppliesbtn").click(function(){
-        $("#cart").hide();
-    });
-    
     $("body").click(function(){
         console.log(event.target);
     });
     $("#b1").click(function(){
         $("#1").clone().appendTo($("#list"));
     });
-    $("#").click(function(){
-        $("#").clone().appendTo($("#list"));
+    $("#b2").click(function(){
+        $("#2").clone().appendTo($("#list"));
     });
-    $("#").click(function(){
-        $("#").clone().appendTo($("#list"));
+    $("#b3").click(function(){
+        $("#3").clone().appendTo($("#list"));
     });
+    $("#b4").click(function(){
+        $("#4").clone().appendTo($("#list"));
+    });
+    $("#b5").click(function(){
+        $("#5").clone().appendTo($("#list"));
+    });
+    $("#b6").click(function(){
+        $("#6").clone().appendTo($("#list"));
+    });
+    $("#b7").click(function(){
+        $("#7").clone().appendTo($("#list"));
+    });
+    $("#b8").click(function(){
+        $("#8").clone().appendTo($("#list"));
+    });
+    $("#b9").click(function(){
+        $("#9").clone().appendTo($("#list"));
+    });
+    $("#b10").click(function(){
+        $("#10").clone().appendTo($("#list"));
+    });
+    $("#b11").click(function(){
+        $("#11").clone().appendTo($("#list"));
+    });
+    $("#b12").click(function(){
+        $("#12").clone().appendTo($("#list"));
+    });
+    $("#b13").click(function(){
+        $("#13").clone().appendTo($("#list"));
+    });
+    $("#b14").click(function(){
+        $("#14").clone().appendTo($("#list"));
+    });
+    $("#b15").click(function(){
+        $("#15").clone().appendTo($("#list"));
+    });
+    $("#b16").click(function(){
+        $("#16").clone().appendTo($("#list"));
+    });
+    $("#b17").click(function(){
+        $("#17").clone().appendTo($("#list"));
+    });
+    $("#b18").click(function(){
+        $("#18").clone().appendTo($("#list"));
+    });
+    $("#b19").click(function(){
+        $("#19").clone().appendTo($("#list"));
+    });
+    $("#b20").click(function(){
+        $("#20").clone().appendTo($("#list"));
+    });
+    $("#b21").click(function(){
+        $("#21").clone().appendTo($("#list"));
+    });
+    $("#b22").click(function(){
+        $("#22").clone().appendTo($("#list"));
+    });
+    $("#b23").click(function(){
+        $("#23").clone().appendTo($("#list"));
+    });
+    $("#b24").click(function(){
+        $("#24").clone().appendTo($("#list"));
+    });
+    var sum = 0;
+    $("#b19").click(function(){
+        var x = document.querySelector(".aPrice").textContent;
+        var tempX = x.substring(1, x.length);
+        var tempXInt = parseFloat(tempX);
+        sum += tempXInt;
+        document.getElementById("subtotal").innerHTML ="$"+ sum;
+        document.getElementById("itemsprice").innerHTML ="$"+ sum;
+    });
+    function sumOfTotal(item) {
+        sum += item;
+        return sum;
+    }
 });
